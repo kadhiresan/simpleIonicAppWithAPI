a87ee22dbd6119d786a881b6024c8df9bb3414d7
kadhiresan <kkadhir10@gmail.com>
2016-07-04 18:06:46 +0530

    added ionicLoading

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -14,21 +14,31 @@ angular.module('starter.controllers', [])
     
 })
 
-.controller('PlanetsCtrl', function($scope, Planets) {
-  Planets.get(function(data){
-    var p =  data.results;
-    var x = [];
-    for (var i = 0; i < p.length; i++) {
-      var item = p[i];
-      (function(item, i){
-        item.id = function(){
-          return i+2
+.controller('PlanetsCtrl', function($scope, Planets, $ionicLoading) {
+    $scope.showLoading = function() {
+        //options default to values in $ionicLoadingConfig
+        $ionicLoading.show().then(function(){
+           console.log("The loading indicator is now displayed");
+        });
+    }();
+
+    Planets.get(function(data){
+        var p =  data.results;
+        var x = [];
+        for (var i = 0; i < p.length; i++) {
+          var item = p[i];
+          (function(item, i){
+            item.id = function(){
+              return i+2
+            }
+            x.push(item);
+          })(item, i)
         }
-        x.push(item);
-      })(item, i)
-    }
-    $scope.planets = x;
-  })
+        $scope.planets = x;
+        $ionicLoading.hide().then(function(){
+           console.log("The loading indicator is now hide");
+        });
+    })
 })
 
 .controller('PlanetDetailCtrl', function($scope, $stateParams, Planets) {
